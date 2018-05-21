46fd8218426b8d393020f24c9605462257f48fa6
Vivoe <justinng367@gmail.com>
2018-05-21 12:40:35 -0400

    Adding Discord parser

--- bot/commands.py
+++ bot/commands.py
@@ -1,15 +1,17 @@
-import argparse
 import shlex
 
+import bot.utils as utils
 
+
+@utils.catch_async_sys_exit
 async def plat_conversion(client, message):
     raw_args = shlex.split(message.content)[1:]
 
-    parser = argparse.ArgumentParser("!plat")
+    parser = utils.DiscordParser("!plat")
     parser.add_argument(
         'plat', type=int, help="Number of plats to convert to CAD.")
 
-    args = parser.parse_args(raw_args)
+    args = await parser.parse_args(client, message.channel, raw_args)
 
     cad = args.plat * (5.49 / 75.0)
 
